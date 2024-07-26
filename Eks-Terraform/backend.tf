f43eee59f9a0f75ca58420b4f06f753e00010def
Manoj kumar Siriparthi <manojs.devops1@gmail.com>
2024-07-26 21:54:18 +0100

    Update backend.tf

--- Eks-Terraform/backend.tf
+++ Eks-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "youtube.nodejs"
+    bucket = "youtube.nodejs" #try with your own bucket
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
